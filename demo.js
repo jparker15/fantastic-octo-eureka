94c15cbea8651d4fab94144dfb1cde17ceabd0f2
Jeremy <jeremyeparker@yahoo.com>
2022-10-11 21:10:03 -0400

    reversered

--- demo.js
+++ demo.js
@@ -1,9 +1,23 @@
-function convertCtoF(celsius) {
-    let fahrenheit = celsius * (9/5) + 32;
+// function convertCtoF(celsius) {
+//     let fahrenheit = celsius * (9/5) + 32;
 
-    console.log(fahrenheit);
+//     console.log(fahrenheit);
 
-    return fahrenheit;
+//     return fahrenheit;
+//   }
+  
+//   convertCtoF(-30);
+
+  function reverseString(str) {
+
+        let a = "";
+        str = str.split("");
+
+      for(let i = str.length - 1; i >= 0; i--){
+            a += str[i];
+      }
+        // console.log(a);
+        return a;
   }
   
-  convertCtoF(-30);
+  reverseString("hello");
