23dbd0e2f75056f88a5e5f287fcc8f14848b43c6
Jermy <jeremyeparker@yahoo.com>
2022-10-12 23:58:10 -0400

    made a factorial

--- fejs.js
+++ fejs.js
@@ -1,15 +1,21 @@
-function convertToF(celsius) {
-    let fahrenheit = celsius * (9/5) + 32;
-    return fahrenheit;
-  }
+// function convertToF(celsius) {
+//     let fahrenheit = celsius * (9/5) + 32;
+//     return fahrenheit;
+//   }
   
-  convertToF(30);
+//   convertToF(30);
 
-  console.log(convertToF(30));
+//   console.log(convertToF(30));
 
   function factorialize(num) {
-      console.log(num);
-    return num;
+      let fax = 1;
+
+      for(let i = 1; i <= num; i++){
+        fax *= i;
+      }
+      console.log(fax);
+      
+    return fax;
   }
   
   factorialize(5)
