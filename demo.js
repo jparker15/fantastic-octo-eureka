27b433011f992a7712fe38821d4203f5427173c9
Jeremy <jeremyeparker@yahoo.com>
2022-10-17 23:54:41 -0400

    largest

--- demo.js
+++ demo.js
@@ -8,16 +8,22 @@
   
 //   convertCtoF(-30);
 
-  function reverseString(str) {
+//   function reverseString(str) {
 
-        let a = "";
-        str = str.split("");
+//         let a = "";
+//         str = str.split("");
 
-      for(let i = str.length - 1; i >= 0; i--){
-            a += str[i];
-      }
-        // console.log(a);
-        return a;
-  }
+//       for(let i = str.length - 1; i >= 0; i--){
+//             a += str[i];
+//       }
+//         // console.log(a);
+//         return a;
+//   }
   
-  reverseString("hello");
+//   reverseString("hello");
+
+function largestOfFour(arr) {
+      return arr;
+    }
+    
+    largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
