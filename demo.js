b77999197b6efb80e2ce9acb3e639bc05f5e685a
jeremy <jeremyeparker@yahoo.com>
2022-10-19 21:52:49 -0400

    cofirm ending

--- demo.js
+++ demo.js
@@ -1,45 +1,51 @@
-// function convertCtoF(celsius) {
-//     let fahrenheit = celsius * (9/5) + 32;
+// // function convertCtoF(celsius) {
+// //     let fahrenheit = celsius * (9/5) + 32;
 
-//     console.log(fahrenheit);
+// //     console.log(fahrenheit);
 
-//     return fahrenheit;
-//   }
+// //     return fahrenheit;
+// //   }
   
-//   convertCtoF(-30);
+// //   convertCtoF(-30);
 
-//   function reverseString(str) {
+// //   function reverseString(str) {
 
-//         let a = "";
-//         str = str.split("");
+// //         let a = "";
+// //         str = str.split("");
 
-//       for(let i = str.length - 1; i >= 0; i--){
-//             a += str[i];
-//       }
-//         // console.log(a);
-//         return a;
-//   }
+// //       for(let i = str.length - 1; i >= 0; i--){
+// //             a += str[i];
+// //       }
+// //         // console.log(a);
+// //         return a;
+// //   }
   
-//   reverseString("hello");
+// //   reverseString("hello");
 
-function largestOfFour(arr) {
-    // console.log(arr[0].length);
-    let dem = [];
+// function largestOfFour(arr) {
+//     // console.log(arr[0].length);
+//     let dem = [];
 
-    for(let i = 0; i < arr.length; i++){
-      let max = arr[i][0];
+//     for(let i = 0; i < arr.length; i++){
+//       let max = arr[i][0];
 
-      for(let j = 0; j < arr[0].length; j++){
-          currentNum = arr[i][j];
+//       for(let j = 0; j < arr[0].length; j++){
+//           currentNum = arr[i][j];
 
-          if (max < currentNum) max = currentNum; 
+//           if (max < currentNum) max = currentNum; 
 
           
-        }
-        dem.push(max);
-      }
-      console.log(dem);
-      return arr;
-    }
+//         }
+//         dem.push(max);
+//       }
+//       console.log(dem);
+//       return arr;
+//     }
     
-    largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+//     largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]);
+
+function confirmEnding(str, target) {
+  return str;
+}
+
+confirmEnding("Bastian", "n");
