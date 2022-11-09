cc79618ede67688f652eaa93f1fa766b2f5dd89f
jeremy <jeremyeparker@yahoo.com>
2022-11-08 22:30:11 -0500

    found it

--- finder.js
+++ finder.js
@@ -1,6 +1,18 @@
 function findElement(arr, func) {
+
     let num = 0;
-    return num;
+
+    for(let i = 0; i < arr.length; i++){
+        let currentNum = arr[i];
+        
+        if (func(currentNum) == true)  {
+            console.log(currentNum);
+            return currentNum
+        }
+        
+    }
+
+    return undefined;
   }
   
-  findElement([1, 2, 3, 4], num => num % 2 === 0); 
+  findElement([1, 3, 5, 8, 9, 10], num => num % 2 === 0); 
